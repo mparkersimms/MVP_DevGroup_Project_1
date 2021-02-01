3f32d730c94794c10825902f611833ec681da6b7
MATTHEW SIMMS <M.PARKER.SIMMS@GMAIL.COM>
2021-02-01 15:19:37 -0600

    added initial functionality

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 'use strict'
+
+var deck = [];
+
+function DeckCreator(source, name) {
+    this.name = name;
+    this.source = source;
+    deckArray.push(this);
+
+}
+var deckArray = [];
+
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+new DeckCreator('https://via.placeholder.com/200', 'name');
+
+
+
+function displayDeck (){
+    console.log('test');
+    var deckElement = document.getElementById('deck');
+    for (var i = 0; i < deckArray.length; i++){
+        var imageElement = document.createElement('img');
+        imageElement.src = deckArray[i].source;
+        deckElement.appendChild(imageElement);
+    }
+}
+
+displayDeck();
+
+
