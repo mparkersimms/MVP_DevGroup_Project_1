7434c8cc0d14585f3a77e9895a7799f6fd73384d
MATTHEW SIMMS <M.PARKER.SIMMS@GMAIL.COM>
2021-02-02 19:58:15 -0600

    final functionality

---
+++ result.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var userNameFromLocalStorage = localStorage.getItem('name');
+var userName = JSON.parse(userNameFromLocalStorage);
+var roundsFromLocalStorage = localStorage.getItem("rounds");
+var roundCount = JSON.parse(roundsFromLocalStorage);
+
+var results = document.getElementById('results');
+var pElement = document.createElement('p');
+pElement.textContent = "You win!";
+var p2Element = document.createElement('p');
+p2Element.textContent = ("Congratulations " + userName + " it took you " + roundCount + " rounds to complete the game!");
+results.appendChild(pElement);
+results.appendChild(p2Element);
