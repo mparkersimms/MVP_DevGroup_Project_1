01f9080ce351bd2228297b5956f047b159c7d1fa
Matthew Simms <76925894+mparkersimms@users.noreply.github.com>
2021-02-05 16:57:56 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,22 +12,22 @@ var DeckCreator = function(source, name1, name2) {
 }
 
 var deckArray = [];
-new DeckCreator('/assets/10.png', 'one1','one');
-new DeckCreator('/assets/10.png', 'one2','one');
-new DeckCreator('/assets/11.png', 'two1','two');
-new DeckCreator('/assets/11.png', 'two2','two');
-new DeckCreator('/assets/12.png', 'three1','three');
-new DeckCreator('/assets/12.png', 'three2','three');
-new DeckCreator('/assets/13.png', 'four1','four');
-new DeckCreator('/assets/13.png', 'four2','four');
-new DeckCreator('/assets/14.png', 'five1','five');
-new DeckCreator('/assets/14.png', 'five2','five');
-new DeckCreator('/assets/15.png', 'six1','six');
-new DeckCreator('/assets/15.png', 'six2','six');
-new DeckCreator('/assets/16.png', 'seven1','seven');
-new DeckCreator('/assets/16.png', 'seven2','seven');
-new DeckCreator('/assets/17.png', 'eight1','eight');
-new DeckCreator('/assets/17.png', 'eight2','eight');
+new DeckCreator('assets/10.png', 'one1','one');
+new DeckCreator('assets/10.png', 'one2','one');
+new DeckCreator('assets/11.png', 'two1','two');
+new DeckCreator('assets/11.png', 'two2','two');
+new DeckCreator('assets/12.png', 'three1','three');
+new DeckCreator('assets/12.png', 'three2','three');
+new DeckCreator('assets/13.png', 'four1','four');
+new DeckCreator('assets/13.png', 'four2','four');
+new DeckCreator('assets/14.png', 'five1','five');
+new DeckCreator('assets/14.png', 'five2','five');
+new DeckCreator('assets/15.png', 'six1','six');
+new DeckCreator('assets/15.png', 'six2','six');
+new DeckCreator('assets/16.png', 'seven1','seven');
+new DeckCreator('assets/16.png', 'seven2','seven');
+new DeckCreator('assets/17.png', 'eight1','eight');
+new DeckCreator('assets/17.png', 'eight2','eight');
 
 
 // This is the function to randomize the deck of cards
